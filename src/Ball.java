ba074765a56422895a2cbdba6bfbcf157d3839ca
Echon <s4mainacc@gmail.com>
2024-07-17 12:08:33 +0200

    Changesr

--- src/Ball.java
+++ src/Ball.java
@@ -3,14 +3,16 @@ public class Ball {
     int ballWidth;
     int ballHeight;
     int ballSpeed;
+    float ballStartX = 650;
+    float ballStartY= 350;
     float ballPosX;
     float ballPosY;
     float speedX;
     float speedY;
 
-    Ball(float startX, float startY, int ballWidth, int ballHeight, int ballSpeed) {
-        this.ballPosX = startX;
-        this.ballPosY = startY;
+    Ball(int ballWidth, int ballHeight, int ballSpeed) {
+        this.ballPosX = ballStartX;
+        this.ballPosY = ballStartY;
         this.ballWidth = ballWidth;
         this.ballHeight = ballHeight;
         this.ballSpeed = ballSpeed;
@@ -31,11 +33,35 @@ public class Ball {
 
     }
 
+    public void reset() {
+         this.ballPosX = ballStartX;
+         this.ballPosY = ballStartY;
+
+    }
+
     public float getBallPosX() {
         return ballPosX;
     }
-
     public float getBallPosY() {
         return ballPosY;
+    }
+
+    public int getBallWidth() {
+        return ballWidth;
+    }
+    public void checkCollision(Player pl1, Player pl2){
+        float deltaXSquared1 = (ballPosX - pl1.playerX) * (ballPosX - pl1.playerX);
+        float deltaYSquared1 = (ballPosY - pl1.playerY) * (ballPosY - pl1.playerX);
+        float distanz1 = (float) Math.sqrt(deltaXSquared1 + deltaYSquared1);
+
+        float deltaXSquared2 = (ballPosX - pl2.playerX) * (ballPosX - pl2.playerX);
+        float deltaYSquared2 = (ballPosY - pl2.playerY) * (ballPosY - pl2.playerX);
+        float distanz2 = (float) Math.sqrt(deltaXSquared2 + deltaYSquared2);
+
+
+        if ((distanz1 <= ballWidth/2) || (distanz2 <= ballWidth/2)){
+            speedX *= -1;
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,62 @@ public class Main extends PApplet {
     Player pl1 = new Player(1);
     Player pl2 = new Player(2);
 
-    float t = 0.0F;
-    float speed = 0.01F;
+    Ball ball = new Ball(35, 35, 2);
 
-    Ball ball = new Ball(650, 350, 35, 35, 2);
+    public static void main(String... args) {
+        PApplet.main("Main");
+    }
+
+    float t = 0.0F;
+    float speed = 0.005F;
+
     int score1 = 0;
     int score2 = 0;
     // Field playingField = new Field(1600, 900);
 
-    public void setup(){
+    public void setup() {
 
-        textSize(25);
+        textSize(40);
         noStroke();
         background(50, 50, 50);
         frameRate(120);
 
     }
+
     public void settings() {
         size(1300, 700);
     }
-    public void draw(){
+
+    public void draw() {
 
         background(50, 50, 50);
         drawPlayer(pl1);
         drawPlayer(pl2);
         drawBall(ball);
         ball.update();
+        ball.checkCollision(pl1, pl2);
 
-        text(str(score1), 10, 25);
-        text(str(score2), 10, 25);
+        fill(255);
+        textAlign(LEFT);
+        text(str(pl1.getScore()), 20, 45);
+        textAlign(RIGHT);
+        text(str(pl2.getScore()), 1300 - 20, 45);
 
+        checkGoal();
     }
+
     public void keyPressed() {
-        if (key == 'w')pl1.moveUp();
-        if (key == 's')pl1.moveDown();
+        if (key == 'w') pl1.moveUp();
+        if (key == 's') pl1.moveDown();
 //        if (key == 'a')pl1.moveLeft();
 //        if (key == 'd')pl1.moveRight();
-        if (keyCode == UP)pl2.moveUp();
-        if (keyCode == DOWN)pl2.moveDown();
+        if (keyCode == UP) pl2.moveUp();
+        if (keyCode == DOWN) pl2.moveDown();
 //        if (keyCode == LEFT)pl2.moveLeft();
 //        if (keyCode == RIGHT)pl2.moveRight();
 
     }
+
     public void drawPlayer(Player pl) {
         // Calculate color values using sine functions
         float playerc1 = (float) (127.5 + 127.5 * sin(t));
@@ -58,7 +72,7 @@ public class Main extends PApplet {
 
         strokeWeight(2);
         stroke(255);
-        rect(pl.x, pl.y, pl.plWidth, pl.plHeight);
+        rect(pl.playerX, pl.playerY, pl.plWidth, pl.plHeight);
     }
 
     public void drawBall(Ball ball) {
@@ -67,7 +81,19 @@ public class Main extends PApplet {
         stroke(255);
         ellipse(ball.ballPosX, ball.ballPosY, ball.ballWidth, ball.ballHeight);
     }
-    public static void main(String... args){
-        PApplet.main("Main");
+
+    public void checkGoal() {
+        if (ball.getBallPosX() <= 0 + ball.getBallWidth()) {
+            scoreGoal(pl2);
+        }
+        if (ball.getBallPosX() >= 1300 - ball.getBallWidth()) {
+            scoreGoal(pl1);
+        }
+    }
+
+    public void scoreGoal(Player pl) {
+        pl.score++;
+        System.out.println("Player " + pl.PlayNr + " scores!");
+        ball.reset();
     }
 }
