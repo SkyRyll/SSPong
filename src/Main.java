4d0d231e4e8ef3cc77b60efc130b5c6394019f7c
SkyRyll <skyryll1987@gmail.com>
2024-07-17 08:46:56 +0200

    changes

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import processing.core.*;
+public class Main extends PApplet {
+    public void settings(){
+        size(200, 200);
+    }
+
+    public void draw(){
+        background(0);
+        ellipse(mouseX, mouseY, 20, 20);
+    }
+
+    public static void main(String... args){
+        PApplet.main("Main");
+    }
+}
